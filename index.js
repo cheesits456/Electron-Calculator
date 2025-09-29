609e71c04589d2c84cac1687722e889596cc8d98
Quin F. <quin@cheesits456.dev>
2025-09-28 19:13:47 -0700

    Change height and width, make it unchangeable

--- index.js
+++ index.js
@@ -5,8 +5,12 @@ const electron = require("electron");
 function createWindow() {
 
 	const window = new electron.BrowserWindow({
-		width: 1200,
-		height: 650,
+		width: 400,
+		height: 600,
+		maxWidth: 400,
+		maxHeight: 600,
+		minWidth: 400,
+		minHeight: 600,
 		icon: path.join(__dirname, "icon", "icon.png"),
 		webPreferences: {
 			contextIsolation: false,
