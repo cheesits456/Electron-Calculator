da22493dd1dc8011a37ea5897388a2c9cae06d35
Quin F. <quin@cheesits456.dev>
2025-09-30 17:01:55 -0700

    Add equate() variable

--- page/script.js
+++ page/script.js
@@ -1,3 +1,5 @@
+const math = require("mathjs");
+
 const equationElement = document.getElementById("equation");
 
 equationElement.focus();
@@ -13,6 +15,18 @@ function clearInput() {
 
 function deleteLastCharacter() {
 	equationElement.value = equationElement.value.substr(0, equationElement.value.length - 1);
+	equationElement.focus();
+};
+
+
+
+function equate() {
+	const equation = equationElement.value
+		.replace(/÷/g, "/")
+		.replace(/×/g, "*")
+		.replace(/−/g, "-");
+
+	equationElement.value = math.evaluate(equation);
 	equationElement.focus();
 };
 
